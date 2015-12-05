10a77b5c97e17a5398b0e72a660b0efe8be95ef5
Luis Miguel Morales Pájaro <luismiguel.mopa@gmail.com>
2015-12-04 22:59:01 -0500

    DetailView and DeleteView
    now you can use de SupraDetailView and SupraDeleteView

--- views.py
+++ views.py
@@ -1,9 +1,10 @@
 from django.views.generic import ListView
-from django.views.generic.edit import FormView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import FormView, DeleteView
 from django.core.serializers.json import DjangoJSONEncoder
 from django.forms.models import inlineformset_factory, modelformset_factory
 from django.http import HttpResponse
-from django.db.models import Q
+from django.db.models import Q, F
 import models
 import json
 
@@ -14,10 +15,16 @@ import json
 	@licence: creative commons
 """
 class SupraListView(ListView):
-	template_name = "supra/json.html"
 	list_display = None
-	paginate_by = 1
-	search_fields = ['nombre']
+	search_fields = []
+	kwargs = {}
+	dict_only = False
+	rules = {}
+
+	def __ini__(self, dict_only = False, *args, **kwargs):
+		self.dict_only = dict_only
+		return super(SupraListView, self).__init__(*args, **kwargs)
+	#end def
 
 	def dispatch(self, request, *args, **kwargs):
 		for field in self.search_fields:
@@ -26,13 +33,13 @@ class SupraListView(ListView):
 				self.kwargs[field] = q
 			#end if
 		#end for
-		return super(SupraListView, self).dispatch(request)
+		return super(SupraListView, self).dispatch(request, *args, **kwargs)
 	#end def
 
 	def get_queryset(self):
 		queryset = super(SupraListView, self).get_queryset()
 		q = Q()
-		for column in self.list_filter:
+		for column in self.search_fields:
 			if column in self.kwargs:
 				search = self.kwargs[column]
 				kwargs = {
@@ -42,27 +49,38 @@ class SupraListView(ListView):
 			#end if
 			queryset = queryset.filter(q)
 		#end for
-		if self.list_display:
-			return list(queryset.values(*self.list_display))
-		#end if
-		return list(queryset.values())
+		queryset = queryset.filter(**self.rules)
+		return queryset
 	#end def
 
 	def get_context_data(self, **kwargs):
 		context = super(SupraListView, self).get_context_data(**kwargs)
-		context['num_rows'] = len(context['object_list'])
+		context['num_rows'] = context['object_list'].count()
 		context['object_list'] = context['object_list']
-		print context
 		return context
 	#end def
 
+	def get_object_list(self):
+		queryset = self.get_queryset()
+		if self.list_display:
+			renderers = dict((key, F(value)) for key, value in self.Renderer.__dict__.iteritems() if not callable(value) and not key.startswith('__'))
+			queryset = queryset.annotate(**renderers)
+			queryset = queryset.values(*self.list_display)
+			object_list = list(queryset)
+		else:
+			object_list = list(queryset.values())
+		#end if
+		return object_list
+	#end def
+
 	def render_to_response(self, context, **response_kwargs):
-		#return super(SupraListView, self).render_to_response(context, **response_kwargs)
 		json_dict = {}
+
+		object_list = self.get_object_list()
+
 		page_obj = context["page_obj"]
 		paginator = context["paginator"]
 		num_rows = context["num_rows"]
-		object_list = context["object_list"]
 		if page_obj:
 			if page_obj.has_previous():
 				json_dict["previous"] = page_obj.previous_page_number()
@@ -78,9 +96,55 @@ class SupraListView(ListView):
 		#end if
 		json_dict["num_rows"] = num_rows
 		json_dict["object_list"] = object_list
+		if self.dict_only:
+			return json_dict
+		#end if
 		return HttpResponse(json.dumps(json_dict, cls=DjangoJSONEncoder), content_type="application/json")
 	#end def
 
+#end class
+
+class SupraDetailView(DetailView):
+	fields = None
+	extra_fields = {}
+	def dispatch(self, request, *args, **kwargs):
+		renderers = dict((key, value) for key, value in self.Renderer.__dict__.iteritems() if not key.startswith('__'))
+		for renderer in renderers:
+			listv = renderers[renderer](dict_only=True)
+			ref = self.get_reference(listv)
+			if ref:
+				pk = kwargs['pk']
+				listv.rules[ref.name] = pk
+				self.extra_fields[renderer] = listv.dispatch(request, *args, **kwargs)
+			#end def
+		return super(SupraDetailView, self).dispatch(request) 
+	#end def
+
+	def get_reference(self, listv):
+		for field in listv.model._meta.fields:
+			if field.is_relation and field.rel.to == self.model:
+				return field
+			#end if
+		#end for
+		return False
+	#end def
+
+	def render_to_response(self, context, **response_kwargs):
+		json_dict = {}
+		if self.fields:
+			for field in self.fields:
+				json_dict[field] = getattr(context["object"], field)
+			#end for
+		else:
+			fields = context["object"]._meta.fields
+			for field in fields:
+				json_dict[field.name] = getattr(context["object"], field.name)
+			#end for
+		#end if
+		for extra in self.extra_fields:
+			json_dict[extra] = self.extra_fields[extra]
+		return HttpResponse(json.dumps(json_dict, cls=DjangoJSONEncoder), content_type="application/json")
+	#enddef
 #end class
 
 class SupraFormView(FormView):
@@ -122,12 +186,12 @@ class SupraFormView(FormView):
 	def is_valid_inlines(self):
 		for inline in self.inlines:
 			i = inline()
-			form = i.get_form_class()
-			f = form(**self.get_form_kwargs())
-			if not f.is_valid():
-				self.invalided_inilines.append(f)
+			form_class = i.get_form_class()
+			form = form_class(**self.get_form_kwargs())
+			if not form.is_valid():
+				self.invalided_inilines.append(form)
 			#end if
-			self.validated_inilines.append(f)
+			self.validated_inilines.append(form)
 		#end for
 		if len(self.invalided_inilines) > 0:
 			return False
@@ -138,13 +202,12 @@ class SupraFormView(FormView):
 	def form_invalid(self, form):
 		errors = dict(form.errors)
 		for i in self.invalided_inilines:
-			errors['inlines'] = dict(i.errors)
+			errors['inlines'] = list(i.errors)
 		#end for
 		return HttpResponse(json.dumps(errors), status=400, content_type="application/json")
 	#end def
 
 #end class
-
 
 class SupraInlineFormView(SupraFormView):
 	base_model = None
@@ -159,4 +222,15 @@ class SupraInlineFormView(SupraFormView):
 			return inlineformset_factory(self.base_model, self.inline_model, exclude=[])
 		#end if
 	#end def
+#end class
+
+class SupraDeleteView(DeleteView):
+	template_name = "supra/delete.html"
+
+	def delete(self, request, *args, **kwargs):
+		self.object = self.get_object()
+		self.object.delete()
+		return HttpResponse(status=200)
+	#end def
+
 #end class
